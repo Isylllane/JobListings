7e11c87c423f8bc653c92ee8f45d7d9e5d6983ca
Isyllane <119064533+Isylllane@users.noreply.github.com>
2024-07-14 00:21:10 +0300

    Add files via upload

---
+++ src/components/JobListing.vue
@@ -0,0 +1,87 @@
+<script setup>
+import { defineProps, ref, computed } from 'vue';
+import { RouterLink } from 'vue-router';
+
+const props = defineProps({
+    job:Object,
+});
+
+const showFullDescription = ref(false);
+const animationDescription = ref(true);
+
+const toggleFullDescription = () => {
+  showFullDescription.value = !showFullDescription.value;
+  animationDescription.value = !animationDescription.value;
+
+}
+const trunvatedDescription = computed(() => {
+  let description = props.job.description;
+  if (!showFullDescription.value) {
+    description = description.substring(0, 100) + '...';
+  }
+  return description
+});
+</script>
+
+<template>
+    
+    <div class="bg-white rounded-xl shadow-md relative">
+            <div class="p-4">
+              <div class="mb-6">
+                <div class="text-gray-600 my-2">{{ job.type }}</div>
+                <h3 class="text-xl font-bold">{{ job.title }}</h3>
+              </div>
+
+              <div class="mb-5"> 
+                <div class="fade transition" :class="animationDescription ? '' : 'in'">
+                  {{ trunvatedDescription }}
+                </div>
+
+                <button @click="toggleFullDescription" class="text-green-500 hover:text-green-600 mb-5">
+                  {{showFullDescription ? 'Less' : 'More'}}
+                </button>
+              </div>
+
+              <h3 class="text-green-500 mb-2">{{ job.salary }} / Year</h3>
+
+              <div class="border border-gray-100 mb-5"></div>
+
+              <div class="flex flex-col lg:flex-row justify-between mb-4">
+                <div class="text-orange-700 mb-3">
+                    <i class="pi pi-map-marker text-orange-700"></i>
+                    {{ job.location }}
+                </div>
+                <RouterLink
+                  :to="'/jobs/'+job.id"
+                  class="h-[36px] bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-center text-sm transition"
+                >
+                  Read More
+                </RouterLink>
+              </div>
+            </div>
+    </div>
+</template>
+<style scoped>
+@keyframes fadeIn { 
+  0% {
+  opacity: 0;
+  }
+  30% {
+  opacity: 0.3;
+  }
+  50% {
+  opacity: 0.5;
+  }
+  70% {
+  opacity: 0.7;
+  }
+  100% {
+  opacity: 1;
+  }
+}
+
+.in {
+  animation: fadeIn 0.4s ease forwards; 
+}
+
+</style>
