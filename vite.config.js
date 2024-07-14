5545a83d77afa297ddd3f690a35e0c2944e2ace1
Isyllane <119064533+Isylllane@users.noreply.github.com>
2024-07-15 00:22:13 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     port: 5734,
     proxy: {
       '/api': {
-        target: 'https://crudcrud.com/api/5d0cdd24e7694f1a8f83d1d1451f0172',
+        target: 'https://vue-jobs-listings.free.beeceptor.com/api',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       }
